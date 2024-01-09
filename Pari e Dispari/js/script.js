f22bd9a9dd6c64590388b582ca99538d2c21084f
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-09 17:46:34 +0100

    create element file html and variable file js

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,8 +1,14 @@
 /*
 1-Creiamo gli elementi che ci servono nel file HTML
-2-Creiamo le variabili per raccogliere gli elementi da file HTML
+2-Creiamo le variabili per raccogliere gli elementi dal file HTML
 3-Genero un numero random tra 1-5 attraverso una funzione
 4-Sommare il numero dell'utente con il numero del computer
 5-Stabilisco con una funzione se il numero è pari o dispari
 6-Dichiaro chi ha vinto in pagina
 */
+
+//2-Creo le variabili per raccogliere gli elementi dal file HTML
+const form = document.querySelector('form')
+const numberUser = document.querySelector('input')
+const result = document.querySelector('h1')
+
