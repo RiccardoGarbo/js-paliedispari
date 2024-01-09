344dcbbfe312d7b1b6b472af71a4de91d9bfbdb2
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-09 19:06:59 +0100

    obtained number, user-cpu end sum

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -2,18 +2,37 @@
 1-Creiamo gli elementi che ci servono nel file HTML
 2-Creiamo le variabili per raccogliere gli elementi dal file HTML
 3-Genero un numero random tra 1-5 attraverso una funzione
-4-Sommare il numero dell'utente con il numero del computer
-5-Stabilisco con una funzione se il numero è pari o dispari
-6-Dichiaro chi ha vinto in pagina
+4-Prendo il valore datomi dall'user attraverso una funzione 
+5-Sommare il numero dell'utente con il numero del computer
+6-Stabilisco con una funzione se il numero è pari o dispari
+7-Dichiaro chi ha vinto in pagina
 */
 
 //2-Creo le variabili per raccogliere gli elementi dal file HTML
 const form = document.querySelector('form')
-const numberUser = document.querySelector('input')
+const inputNumberUser = document.querySelector('input')
 const result = document.querySelector('h1')
 
 //3-Genero un numero random tra 1-5 attraverso una funzione
 function randomNumber() {
-    return Math.floor(Math.random() * 5) + 1
+    return parseInt(Math.floor(Math.random() * 5) + 1)
 }
+//4-Prendo il valore datomi dall'user attraverso una funzione 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let userNumber = parseInt(inputNumberUser.value)
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+        alert('Inserisci un numero da 1 a 5')
+        return;
+    }
+    console.log(userNumber, 'numero user')
+    const numberCpu = randomNumber()
+    console.log(numberCpu, 'numero cpu')
+    //5-Sommare il numero dell'utente con il numero del computer
+    const sum = userNumber + numberCpu
+    console.log(sum, 'somma numeri cpu e user')
+
+
+})
+
 
