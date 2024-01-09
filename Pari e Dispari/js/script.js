9cdf55721ccf9ca0a862f484692771be760e0668
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-09 17:25:55 +0100

    TODO

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,8 @@
+/*
+1-Creiamo gli elementi che ci servono nel file HTML
+2-Creiamo le variabili per raccogliere gli elementi da file HTML
+3-Genero un numero random tra 1-5 attraverso una funzione
+4-Sommare il numero dell'utente con il numero del computer
+5-Stabilisco con una funzione se il numero è pari o dispari
+6-Dichiaro chi ha vinto in pagina
+*/
